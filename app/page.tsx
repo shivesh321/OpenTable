276c4463e2c41f53b63a2ea6a58a56124db675a8
Shivesh kr jha <shiveshkrjha2015@gmail.com>
2023-09-12 16:40:10 +0530

    install Tailwind CSS

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>Hello my friends</div>
+      <h1 className="text-blue-400 text-7xl">Hello my friends</h1>
     </main>
   );
 }
